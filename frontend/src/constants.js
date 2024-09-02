be9aa55a45025e223d8de67242697f132e0d8654
krisna-chan <151432350+krisna-chan@users.noreply.github.com>
2024-09-02 11:07:51 +0700

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '/';
+  process.env.NODE_ENV === 'develeopment' ? 'pro-shop-4skywkujv-luck2.vercel.app' : '/';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
