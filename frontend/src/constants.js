d37432a8f17280efe121a3e940abaacb62b8aac0
krisna-chan <151432350+krisna-chan@users.noreply.github.com>
2024-09-02 11:11:53 +0700

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'develeopment' ? 'pro-shop-4skywkujv-luck2.vercel.app' : '/';
+  process.env.NODE_ENV === 'develeopment' ? 'https://pro-shop-topaz.vercel.app/' : '/';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
